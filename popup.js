905cb7b3c29d58f3c4721e096dfa10b516ee3ddf
dberinger <danielberinger95@gmail.com>
2023-09-18 14:52:34 +0200

    Refactor popup.js

--- popup.js
+++ popup.js
@@ -2,120 +2,119 @@ const shopeeBtn = document.getElementById("shopee-icon");
 const openTabsP = document.getElementById("open-tabs");
 const copyBtn = document.getElementById("copy-btn");
 const textArea = document.getElementById("textarea");
-const activeWindowCheckBox = document.getElementById('active-window')
+const activeWindowCheckBox = document.getElementById("active-window");
+const productRegexes = [
+  /-i[.]\d*[.]\d*[?]sp_atk/,
+  /product[/]\d*[/]\d*/,
+  /-i[.]\d*[.]\d*/,
+];
 
-const openTabsNum = new Promise((res) => {
-
-    let regexList = [/-i[.]\d*[.]\d*[?]sp_atk/, /product[/]\d*[/]\d*/, /-i[.]\d*[.]\d*/];
-
-    chrome.tabs.query({}, function (tabs) {
-        let productTabs = 0;
-        tabs.forEach(function (tab) {
-            if (regexList.some(rx => rx.test(tab.url))) {
-                productTabs++;
-            }
-        })
-        res(productTabs);
-    })
-})
+// count number of tabs with products open - dependent on url regex match
+const openTabsNum = new Promise((resolve, reject) => {
+  chrome.tabs.query({}, function (tabs) {
+    let productTabs = 0;
+    tabs.forEach(function (tab) {
+      if (productRegexes.some((rx) => rx.test(tab.url))) {
+        productTabs++;
+      }
+    });
+    resolve(productTabs);
+  });
+});
 
 async function getTabsUrl(activeOnly) {
-    return new Promise((resolve, reject) => {
-        if(activeOnly == true) {
-            chrome.tabs.query({
-                currentWindow: activeOnly
-            }, function (tabs) {
-                let arr = [];
-                tabs.forEach(function (tab) {
-                    arr.push(tab.url);
-                })
-                resolve(arr);
-            })
-        } else {
-            chrome.tabs.query({}, function (tabs) {
-                let arr = [];
-                tabs.forEach(function (tab) {
-                    arr.push(tab.url);
-                })
-                resolve(arr);
-            })
+  return new Promise((resolve, reject) => {
+    if (activeOnly) {
+      chrome.tabs.query(
+        {
+          currentWindow: activeOnly,
+        },
+        function (tabs) {
+          let arr = [];
+          tabs.forEach(function (tab) {
+            arr.push(tab.url);
+          });
+          resolve(arr);
         }
-    })
+      );
+    } else {
+      chrome.tabs.query({}, function (tabs) {
+        let arr = [];
+        tabs.forEach(function (tab) {
+          arr.push(tab.url);
+        });
+        resolve(arr);
+      });
+    }
+  });
 }
 
 function cleanUrl(url) {
-    try {
-        return url.split("-i.")[1].split("?sp_atk")[0].split('.');
-    } catch (e) {
-        try {
-            return url.split("product/")[1].split("/");
-        } catch (e) {
-            try {
-                return url.split("-i.")[1].split(".");
-            } catch (e) {
-                return null;
-            }
-        }
-    }
+  try {
+    return url
+      .split(/-i\.|product\//)[1]
+      .split("?sp_atk")[0]
+      .split(".");
+  } catch (e) {
+    return null;
+  }
 }
 
 function genShopItemIds(urls, reverse_order) {
-    let arr = [];
-    console.log(urls);
-    urls.forEach(url => {
-        let ids = cleanUrl(url);
-        if (ids !== null) {
-            arr.push(ids)
-        };
-    })
-    console.log(arr);
-    if (reverse_order == true) {
-        arr = arr.reverse();
+  let arr = [];
+
+  urls.forEach((url) => {
+    let ids = cleanUrl(url);
+    if (ids !== null) {
+      arr.push(ids);
     }
-    return arr.map(lines => lines.join("\t")).join("\n");
+  });
+
+  if (reverse_order) {
+    arr = arr.reverse();
+  }
+
+  return arr.map((lines) => lines.join("\t")).join("\n");
 }
 
 copyBtn.addEventListener("click", async () => {
-    let isReverseChecked = document.getElementById('reverse-order').checked
-    let isActiveWindowChecked = document.getElementById('active-window').checked
-    const ids = genShopItemIds(await getTabsUrl(isActiveWindowChecked), isReverseChecked);
-    textArea.textContent = ids;
-    textArea.select();
-    navigator.clipboard.writeText(textArea.textContent);
-    if (ids.length !== 0) {
-        textArea.textContent = "Copied!";
-    } else {
-        textArea.textContent = "Nothing to be copied :(";
-    }
+  let isReverseChecked = document.getElementById("reverse-order").checked;
+  let isActiveWindowChecked = document.getElementById("active-window").checked;
+  const ids = genShopItemIds(
+    await getTabsUrl(isActiveWindowChecked),
+    isReverseChecked
+  );
+  textArea.textContent = ids;
+  textArea.select();
+  navigator.clipboard.writeText(textArea.textContent);
+  textArea.textContent =
+    ids.length != 0 ? "Copied!" : "Nothing to be copied :(";
 });
 
 activeWindowCheckBox.addEventListener("change", async (e) => {
-    if (e.target.checked == true) {
-        let currentOpenTabsNum = new Promise((res) => {
-
-            let regexList = [/-i[.]\d*[.]\d*[?]sp_atk/, /product[/]\d*[/]\d*/, /-i[.]\d*[.]\d*/];
-
-            chrome.tabs.query({
-                currentWindow: true
-            }, function (tabs) {
-                let productTabs = 0;
-                tabs.forEach(function (tab) {
-                    if (regexList.some(rx => rx.test(tab.url))) {
-                        productTabs++;
-                    }
-                })
-                res(productTabs);
-            })
-        })
-        openTabsP.innerText = "Open products: " + await currentOpenTabsNum
-        console.log('checked')
-    } else {
-        openTabsP.innerText = "Open products: " + await openTabsNum
-        console.log('unchecked')
-    }
+  if (e.target.checked) {
+    let currentOpenTabsNum = new Promise((resolve) => {
+      chrome.tabs.query(
+        {
+          currentWindow: true,
+        },
+        function (tabs) {
+          let productTabs = 0;
+          tabs.forEach(function (tab) {
+            if (productRegexes.some((rx) => rx.test(tab.url))) {
+              productTabs++;
+            }
+          });
+          resolve(productTabs);
+        }
+      );
+    });
+    openTabsP.innerText = "Open products: " + (await currentOpenTabsNum);
+  } else {
+    openTabsP.innerText = "Open products: " + (await openTabsNum);
+  }
 });
 
-
-((async () => {
-    openTabsP.innerText = "Open products: " + await openTabsNum;
-})()).catch(console.error);
+(async () => {
+  openTabsP.innerText = "Open products: " + (await openTabsNum);
+})().catch(console.error);
